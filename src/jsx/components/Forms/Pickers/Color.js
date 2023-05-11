2e91d516657196f7dadb661bbebdaa0d82f1fe24
Trieu <vokhactrieu91@gmail.com>
2023-05-12 00:38:27 +0700

    deploy 2.3

--- src/jsx/components/Forms/Pickers/Color.js
+++ src/jsx/components/Forms/Pickers/Color.js
@@ -1,17 +1,11 @@
 import React, { useState } from "react";
 
-import ColorPicker_ from "material-ui-color-picker";
 
 const ColorPicker = () => {
    const [color, setColor] = useState("");
 
    return (
-      <ColorPicker_
-         name="color"
-         defaultValue="Color"
-         value={color}
-         onChange={(color) => setColor(color)}
-      />
+      <div></div>
    );
 };
 
