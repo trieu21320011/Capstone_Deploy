8f57c44511e53825f4c88fdae4930093259b179b
Trieu <vokhactrieu91@gmail.com>
2023-05-12 01:20:30 +0700

    deploy 2.7

--- src/jsx/components/Forms/Summernote/Rte.js
+++ src/jsx/components/Forms/Summernote/Rte.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { EditorState, ContentState } from "draft-js";
-import { Editor } from "react-draft-wysiwyg";
 import htmlToDraft from "html-to-draftjs";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 class EditorConvertToHTML extends Component {
    constructor(props) {
@@ -30,12 +28,7 @@ class EditorConvertToHTML extends Component {
       const { editorState } = this.state;
       return (
          <div>
-            <Editor
-               editorState={editorState}
-               wrapperClassName="demo-wrapper"
-               editorClassName="demo-editor"
-               onEditorStateChange={this.onEditorStateChange}
-            />
+            
          </div>
       );
    }
